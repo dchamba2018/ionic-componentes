8d423406c267665a765ddaf3f8dd5e502c1ec27b
Diego Chamba <dchamba@live.com>
2022-01-11 19:46:03 -0500

    git activo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+    console.log("Abriendo mi app");
+    let num=10;
+    num=20;
+    num=num+10;
+    num=1;
+    console.log(num);
+  }
 
 }
